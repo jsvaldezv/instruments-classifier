45d29c6349f69d32e5fed60243e7d3d90ebbc771
Jesús Valdez <jsvaldezv@yahoo.com>
2022-01-28 22:11:31 -0600

    Add export model function

---
+++ Utilities/LoadAudio.py
@@ -0,0 +1,51 @@
+import soundfile as sf
+from soundfile import SoundFile
+
+# CHECAR MAYOR LONGITUD DE TODOS LOS AUDIOS
+def checkStems(inPaths):
+    mayor = 0
+    #for path in inPaths:
+    audio, sr = sf.read(inPaths)
+    if len(audio) > mayor:
+        mayor = len(audio)
+        
+    return mayor
+
+
+# LOAD AUDIOS
+def loadAudio(inPath, inConfig="mono"):
+    print("---------")
+    print("Añadiendo tracks...")
+
+    track = []
+    sampleRate = 0
+
+    audio = SoundFile(inPath)
+    
+    if inConfig == "mono":
+        if audio.channels == 1:
+            samples, sr = sf.read(inPath)
+            track = samples
+            sampleRate = sr
+
+        else:
+            samples, sr = sf.read(inPath)
+            track = samples[:,0]
+            sampleRate = sr
+
+    elif inConfig == "stereo":
+        if audio.channels == 1:
+            track = []
+            samples, sr = sf.read(inPath)
+
+            for sample in samples:
+                track.append([sample, sample])
+
+            sampleRate = sr
+
+        else:
+            samples, sr = sf.read(inPath)
+            track = samples
+            sampleRate = sr
+
+    return track, sampleRate
---
+++ main.py
@@ -0,0 +1,9 @@
+import Utilities.LoadAudio as load
+import pickle
+
+
+
+
+#model = pickle.load(open('Models/randomForest_Model.sav', 'rb'))
+#result = model.score(X_test, Y_test)
+#print(result)
